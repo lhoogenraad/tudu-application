35a08e70c63b727d003882c80ad9ce815773f999
Leon <le.o.n@outlook.com>
2020-10-28 18:06:04 +1300

    can confirm links to view task now redirect user to correct task id api link

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,12 +1,15 @@
 export default {
     tasks: [
         {
+            id: '123',
             name: 'Do Dishes',
             date: '12/10/2020'
         },{
+            id: '456',
             name: 'Empty bin',
             date: '13/10/2020'
         },{
+            id: '789',
             name: 'Wash car',
             date: '13/10/2020'
         }
