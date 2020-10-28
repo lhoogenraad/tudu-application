a993df6c347e9815bf01587c367b6988097b0fd7
Leon <le.o.n@outlook.com>
2020-10-28 18:18:53 +1300

    navigating to address/tasks/__taskid now shows the details of a task

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -3,15 +3,18 @@ export default {
         {
             id: '123',
             name: 'Do Dishes',
-            date: '12/10/2020'
+            date: '12/10/2020',
+            description: 'Put water on dishes and stuff',
         },{
             id: '456',
             name: 'Empty bin',
-            date: '13/10/2020'
-        },{
+            date: '13/10/2020',
+            description: 'put bin contents in bigger bin outside',
+        },{ 
             id: '789',
             name: 'Wash car',
-            date: '13/10/2020'
+            date: '13/10/2020',
+            description: 'put water on car and soap it up',
         }
     ]
 }
--- frontend/src/screens/taskscreen.js
+++ frontend/src/screens/taskscreen.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import data from '../data';
 
-function taskscreen (props) {
-    return <div>taskscreen!</div>
+function taskscreen(props) {
+    console.log(props.match.params.id);
+    const task = data.tasks.find(x => x.id === props.match.params.id);
+    return (
+        <div>
+            <div>
+                <Link to="/">Back to your tasks</Link>
+            </div>
+            <div className="details">
+                <div className="details-description">
+                    <ul>
+                        <li>
+                            <h1>{task.name}</h1>
+                        </li>
+                        <li>
+                            <p>{task.description}</p>
+                        </li>
+                        <li>
+                            <p>Created on {task.date}</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default taskscreen;
