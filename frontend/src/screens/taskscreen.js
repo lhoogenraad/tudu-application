a4040260d00da6296765c2e01364d5352f3caf0e
Leon <le.o.n@outlook.com>
2020-11-02 17:09:56 +1300

    Edited html return code in taskscreen.js, now page either displays loading..., error message or actual content

--- frontend/src/screens/taskscreen.js
+++ frontend/src/screens/taskscreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import { getTask } from '../actions/taskActions';
 
@@ -8,7 +8,7 @@ function Taskscreen(props) {
 
     /* These are our react hook definitions */
     const taskData = useSelector(state => state.taskData);
-    const {task, loading, error} = taskData;
+    const { task, loading, error } = taskData;
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -23,24 +23,29 @@ function Taskscreen(props) {
             <div>
                 <Link to="/" className="back-button"><span>Back to your tasks</span></Link>
             </div>
-            <div className="details">
-                <div className="details-description">
-                    <ul>
-                        <li>
-                            <h1>{task.name}</h1>
-                        </li>
-                        <li>
-                            <p>{task.description}</p>
-                        </li>
-                        <li>
-                            <p>Created on {task.date}</p>
-                        </li>
-                    </ul>
-                </div>
-                <div className="details-actions">
-                    <button className="deletebutton">Delete task</button>
-                </div>
-            </div>
+            {loading ? <div>Loading...</div> :
+                error ? <div>{error}</div> :
+                    (
+                        <div className="details">
+                            <div className="details-description">
+                                <ul>
+                                    <li>
+                                        <h1>{task.name}</h1>
+                                    </li>
+                                    <li>
+                                        <p>{task.description}</p>
+                                    </li>
+                                    <li>
+                                        <p>Created on {task.date}</p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="details-actions">
+                                <button className="deletebutton">Delete task</button>
+                            </div>
+                        </div>
+                    )
+            }
         </div>
     )
 }
