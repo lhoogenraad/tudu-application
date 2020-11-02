79a05088791b2affa1d72ba396292d9677c29051
Leon <le.o.n@outlook.com>
2020-11-02 16:37:45 +1300

    Created rd task reducers file

---
+++ frontend/src/reducers/taskReducers.js
@@ -0,0 +1,16 @@
+
+
+function taskListReducer(state= {tasks: []}, action){
+    switch (action.type){
+        case TASK_LIST_REQUEST:
+            return {loading: true};
+        case TASK_LIST_SUCCESS:
+            return {loading: false, tasks: action.payload};
+        case TASK_LIST_FAIL:
+            return {loading: false, error: action.payload};
+        default:
+            return state;
+    }
+}
+
+export {taskListReducer};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,5 @@
 import {createStore, combineReducers} from 'redux';
-
+import {taskListReducer} from './reducers/taskReducers';
 
 
 const initialState = {};
