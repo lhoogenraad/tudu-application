982ff0783530ff2f78de9cc3691faf4fa0005e63
Leon <le.o.n@outlook.com>
2020-11-02 17:05:48 +1300

    Have written out code for taskActions.js getTask() method, now need to write up reducer and constans

--- frontend/src/actions/taskActions.js
+++ frontend/src/actions/taskActions.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 import {TASK_LIST_FAIL, TASK_LIST_REQUEST, TASK_LIST_SUCCESS} from '../constants/taskConstants';
 
+// Returns all tasks in data file
 const listTasks = () =>  async (dispatch) => {
     try{
         dispatch({type: TASK_LIST_REQUEST});
@@ -8,6 +9,17 @@ const listTasks = () =>  async (dispatch) => {
         dispatch({type: TASK_LIST_SUCCESS, payload: data});
     }catch(error){
         dispatch({type: TASK_LIST_FAIL, payload: error.message});
+    }
+}
+
+// Returns specified task (specified by id field)
+const getTask = (taskID) => async (dispatch) => {
+    try{
+        dispatch({type: TASK_SELECT_REQUEST, payload: taskID});
+        const {data} = await Axios.get('/api/tasks/' + taskID);
+        dispatch({type: TASK_SELECT_SUCCESS, payload: data});
+    }catch(error){
+        dispatch({type: TASK_SELECT_FAIL, payload: error.message});
     }
 }
 
