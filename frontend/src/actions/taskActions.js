6d9a7bbd2a026ec5a615366628c17084e163b6fd
Leon <le.o.n@outlook.com>
2020-11-02 16:50:23 +1300

    Finished taskActions.js file

--- frontend/src/actions/taskActions.js
+++ frontend/src/actions/taskActions.js
@@ -1,12 +1,14 @@
 import Axios from 'axios';
-import {TASK_LIST_REQUEST, TASK_LIST_SUCCESS} from '../constants/taskConstants';
+import {TASK_LIST_FAIL, TASK_LIST_REQUEST, TASK_LIST_SUCCESS} from '../constants/taskConstants';
 
 const listTasks = () => (dispatch) => {
     try{
         dispatch({type: TASK_LIST_REQUEST});
         const {data} = await Axios.get('/api/tasks');
         dispatch({type: TASK_LIST_SUCCESS, payload: data});
-    }catch(){
-
+    }catch(error){
+        dispatch({type: TASK_LIST_FAIL, payload: error.message});
     }
 }
+
+export {listTasks};
