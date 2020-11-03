7497e61d705226d7aed7556ef5a0c4b0f36a4ab6
Leon <le.o.n@outlook.com>
2020-11-04 12:55:41 +1300

    Finished userModel.js file, now starting on userRoute.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,1 +1,13 @@
 import mongoose from 'mongoose';
+
+// Describes how a user's data will be saved to the DB
+const userSchema = new mongoose.Schema({
+    name: {type: String, require: true},
+    email: {type: String, require: true, unique: true},
+    password: {type: String, require: true},
+});
+
+// Defines the name of the model as 'User', and uses the userSchema previously defined
+const userModel = mongoose.model('User', userSchema);
+
+export default userModel;
