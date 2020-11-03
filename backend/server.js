bacc5c4239aa19a8c42705711d39df28a8db90bd
Leon <le.o.n@outlook.com>
2020-11-04 12:45:02 +1300

    Have now installed mongoose and use it to connect to the db, now need to fix error in config.js, and begin working on models of objects

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import data from './data';
 import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+
 
 dotenv.config();
 
 const mongodbUrl = config.mongodbUrl;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true
+}).catch(error => console.log(error.reason));
 
 const app = express();
 
