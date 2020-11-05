4368adf1fda7da7cccf4c4eca63ea0d235fafee9
Leon <le.o.n@outlook.com>
2020-11-05 21:30:12 +1300

    Currently not able to connect to db, because authentication fails

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://l.e.o.n@hotmail.co.nz:organisedrice123@cluster0.adkpp.mongodb.net/TuDu?retryWrites=true&w=majority',
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://l.e.o.n@hotmail.co.nz:organisedrice123@cluster0.adkpp.mongodb.net/tudu?retryWrites=true&w=majority',
     JWT_SECRET: process.env.JWT_SECRET || 'secretkey'
 }
